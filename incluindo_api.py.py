5520d8a05a65f4f604bf5a0a0ff1c193346d0d99
viniciusMora-developer <160442198+viniciusMora-developer@users.noreply.github.com>
2024-03-01 12:13:31 -0300

    Update incluindo_api.py.py

--- incluindo_api.py.py
+++ incluindo_api.py.py
@@ -1,18 +1,19 @@
-import re
-import json
 import requests
 import sys
+try:
+    cep=int(input("Digite seu CEP: "))
 
-cep=int(input("Digite seu CEP: "))
-
-infor= requests.get(f"https://viacep.com.br/ws/{cep}/json/")
-infor= infor.json()
-print("cep: ",infor["cep"])
-print("logradouro: ",infor["logradouro"])
-print("complemento: ",infor["complemento"])
-print("bairro: ",infor["bairro"])
-print("localidade: ",infor["localidade"])
-print("uf: ",infor["uf"])
-print("ibge: ",infor["ibge"])
-print("ddd: ",infor["ddd"])
-print("siafi: ",infor["siafi"])
+    infor= requests.get(f"https://viacep.com.br/ws/{cep}/json/")
+    infor= infor.json()
+    print("cep: ",infor["cep"])
+    print("logradouro: ",infor["logradouro"])
+    print("complemento: ",infor["complemento"])
+    print("bairro: ",infor["bairro"])
+    print("localidade: ",infor["localidade"])
+    print("uf: ",infor["uf"])
+    print("ibge: ",infor["ibge"])
+    print("ddd: ",infor["ddd"])
+    print("siafi: ",infor["siafi"])
+except requests.RequestException as e:
+    print("Erro ao tentar localizar CEP: ",e)
+    sys.exit()
